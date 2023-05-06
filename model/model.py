3f750186835651f4173aebc752e2c7b53b5a9b18
renbrent <renbrent23@gmail.com>
2023-05-06 14:09:58 -0700

    modified model.py

--- model/model.py
+++ model/model.py
@@ -1,1 +1,13 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from utils import *
+
+
+dataset = get_test_images()
+
+print(dataset)

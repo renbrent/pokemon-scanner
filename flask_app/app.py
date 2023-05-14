ff21aed92827e481b6251db29e97177744740165
renbrent <renbrent23@gmail.com>
2023-05-13 18:12:46 -0700

    added predict endpoint

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from model.model import get_predictions
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/predict', methods=["GET"])
+def predict():
+    return jsonify(get_predictions('../model/dataset',
+                           '../model/test_dataset',
+                           '../model/model_weights.pth'))
 
 if __name__ == '__main__':
     app.run()

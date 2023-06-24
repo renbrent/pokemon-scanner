0da655bd0a0efdb68152b2e9176c3dd0b749536a
renbrent <renbrent23@gmail.com>
2023-06-23 22:25:12 -0700

    added endpoints and wtforms

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,50 @@
-from flask import Flask, jsonify
-from model.model import get_predictions
+import os
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import CSRFProtect, FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import SubmitField
+from werkzeug.utils import secure_filename
+from model.model import model_predict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+UPLOAD_FOLDER = "./static/uploads"
+
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+csrf = CSRFProtect(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/predict', methods=["GET"])
+class ImageForm(FlaskForm):
+    image = FileField(validators=[FileRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/")
+def home():
+    form = ImageForm()
+    return render_template("index.html", form=form)
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    return jsonify(get_predictions('../model/dataset',
-                           '../model/test_dataset',
-                           '../model/model_weights.pth'))
+    form = ImageForm()
 
-if __name__ == '__main__':
+    if form.validate_on_submit():
+        f = form.image.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+        result = model_predict(filename)
+
+        return render_template("predict.html", results=result, image_path=filename)
+
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
     app.run()

90b0cfe850b78d2c1d9371b7fdd3050b5035893c
renbrent <renbrent23@gmail.com>
2023-06-23 22:26:12 -0700

    added predict function

--- model/model.py
+++ model/model.py
@@ -1,26 +1,26 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from .utils import *
+from model.utils import *
+from .labels import POKEMON_NAMES
 
-def get_predictions(data_dir="./model/dataset",
-                    test_dir="./model/test_dataset",
-                    weight_dir="./model/model_weights.pth"):
+WEIGHT_DIR = "../model/model_weights.pth"
+UPLOADS = "../flask_app/static/uploads/"
 
+
+def get_predictions(test_dir="./model/test_dataset"):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    labels_to_name = get_labels(data_dir)
 
     dataset, _ = get_test_images(test_dir)
     model = models.resnet50()
     num_ftrs = model.fc.in_features
 
-    model.fc = nn.Linear(num_ftrs, 150) #No. of classes = 150
+    model.fc = nn.Linear(num_ftrs, 150)  # No. of classes = 150
     model = model.to(device)
-    model.load_state_dict(torch.load(weight_dir))
+    model.load_state_dict(torch.load(WEIGHT_DIR))
     model.eval()
 
-    predicts=[]
+    predicts = []
 
     for i in range(len(dataset[:10])):
         data = dataset[i].cuda()
@@ -28,13 +28,31 @@ def get_predictions(data_dir="./model/dataset",
         conf_10, predicted_10 = torch.topk(output.data, k=10, dim=1)
 
         conf_10, predicted_10 = conf_10.tolist(), predicted_10.tolist()
-        labels = [labels_to_name[key] for key in predicted_10[0]]
+        labels = [POKEMON_NAMES[key] for key in predicted_10[0]]
         confs = [conf for conf in conf_10[0]]
-        predicts.append({
-            "Test": f"{i+1}",
-            "Top 10": labels,
-            "Confidence Levels": confs
-        })
+        predicts.append({"Top 10": labels, "Confidence Levels": confs})
     return predicts
 
 
+def model_predict(image):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    image = process_image(f"{UPLOADS}{image}")
+    model = models.resnet50()
+    num_ftrs = model.fc.in_features
+
+    model.fc = nn.Linear(num_ftrs, 150)  # No. of classes = 150
+    model = model.to(device)
+    model.load_state_dict(torch.load(WEIGHT_DIR))
+    model.eval()
+
+    data = image.cuda()
+    output = model(data)
+    conf_10, predicted_10 = torch.topk(output.data, k=10, dim=1)
+
+    conf_10, predicted_10 = conf_10.tolist(), predicted_10.tolist()
+    labels = [POKEMON_NAMES[key] for key in predicted_10[0]]
+    confs = [conf for conf in conf_10[0]]
+    predict = {"Top 10": labels, "Confidence Levels": confs}
+
+    return predict
